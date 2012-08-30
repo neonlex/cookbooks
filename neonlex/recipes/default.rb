3153892b8ef3bb0f0deeb788ea2805d0cc37e9aa
Daniel Spangenberg <danielsp@amazon.com>
2012-08-30 11:20:18 +0200

    add official tmux and zsh cookbooks

--- neonlex/recipes/default.rb
+++ neonlex/recipes/default.rb
@@ -1,19 +1,2 @@
-package 'tmux' do
-  action :install
-end
-
-package 'zsh' do
-  action :install
-end
-
-git node[:neonlex][:dotfiles][:directory] do
-  repository 'https://github.com/neonlex/dotfiles.git'
-  reference 'master'
-  action :sync
-end
-
-execute 'install dotfiles' do
-  command 'yes | rake install'
-  cwd node[:neonlex][:dotfiles][:directory]
-  action :run
-end
+include_recipe "zsh::default"
+include_recipe "tmux::default"
